3f3d98408f1f1c3e85419394db28eca95bf2ee72
Soumya Ranjan Nanda <srnranjan2005@gmail.com>
2026-07-22 16:42:26 +0530

    feat(user): implement user domain model

---
+++ src/main/java/com/eventflow/eventflow/entity/User.java
@@ -0,0 +1,52 @@
+    package com.eventflow.eventflow.entity;
+
+    import jakarta.persistence.*;
+    import jakarta.validation.constraints.NotBlank;
+    import lombok.AllArgsConstructor;
+    import lombok.Getter;
+    import lombok.NoArgsConstructor;
+    import lombok.Setter;
+
+    import java.time.Instant;
+    import java.time.LocalDate;
+    import java.util.UUID;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    @Entity
+    @Table(name = "users")
+    public class User {
+
+        @Id
+        private UUID id;
+
+        @Column(nullable = false)
+        private String firstName;
+
+        @Column(nullable = false)
+        private String lastName;
+
+        @NotBlank
+        @Column(nullable = false)
+        private String password;
+
+        @Column(nullable = false, unique = true)
+        private String email;
+
+        @Column(nullable = false, unique = true)
+        private String phoneNumber;
+
+        private Instant createdAt;
+        private Instant updatedAt;
+
+        @Enumerated(EnumType.STRING)
+        private Role role;
+
+        private boolean enabled;
+
+        @Column(nullable = false)
+        private LocalDate dateOfBirth;
+
+    }
