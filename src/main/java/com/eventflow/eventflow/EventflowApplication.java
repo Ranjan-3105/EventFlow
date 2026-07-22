54c4d006ece3e99bf3e33e92e7e1725cbc50d341
Soumya Ranjan Nanda <srnranjan2005@gmail.com>
2026-07-22 15:46:21 +0530

    feat: bootstrap EventFlow backend with Spring Boot and PostgreSQL

--- src/main/java/com/eventflow/eventflow/EventflowApplication.java
+++ src/main/java/com/eventflow/eventflow/EventflowApplication.java
@@ -1,5 +1,7 @@
 package com.eventflow.eventflow;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EventflowApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(EventflowApplication.class, args);
 	}
 
